1896a21bec0431df1fa14b562e477fbae9350190
poorlydefinedbehaviour <brunotj2015@hotmail.com>
2026-04-14 19:20:34 -0300

    another typo from local debugging

--- migrations/20260413202347_create_tables.sql
+++ migrations/20260413202347_create_tables.sql
@@ -8,5 +8,5 @@ CREATE TABLE tasks (
     execution_time_at timestamptz NOT NULL
 );
 
-CREATE INDEX idx_tasks_execution_time_at ON tasks (state, execution_time_at) WHERE tasks.state = 'pending';
+CREATE INDEX idx_tasks_execution_time_at ON tasks (state, execution_time_at) WHERE tasks.state = '1';
 CREATE INDEX idx_tasks_state ON tasks (state);
